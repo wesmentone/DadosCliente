6ca56838dd249e39cec62f02df21cdcc63e7e2e1
wesley <wesleymentone17@gmail.com>
2025-07-22 20:29:03 -0300

    dados cliente

---
+++ trabalho 1 python 1.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura (em metros): "))
+
+print(f"\nOlá, {nome}! Você tem {idade} anos e mede {altura} metros."
